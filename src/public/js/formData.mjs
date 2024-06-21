5b99a18dda2d2f58168aac7991988c9a5fae995c
pascalspencer <pascalouma74480@gmail.com>
2024-06-21 19:44:35 +0300

    websocket frontend

--- src/public/js/formData.mjs
+++ src/public/js/formData.mjs
@@ -1,5 +1,4 @@
 import DerivAPIBasic from 'https://cdn.skypack.dev/@deriv/deriv-api/dist/DerivAPIBasic';
-import WebSocket from 'ws';
 
 document.addEventListener("DOMContentLoaded", function () {
   const dataForm = document.getElementById("trade-form");
